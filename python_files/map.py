9b78c112d9ebf6ba0334c74d28afb205af904ec0
Tao <tao.spa2003@gmail.com>
2022-01-22 14:15:44 +0100

    putting python files into a specified folder + fix 'convention errors'

--- python_files/map.py
+++ python_files/map.py
@@ -2,10 +2,11 @@ import pygame
 from tiles import Tile
 from settings import tile_size
 
+
 class Map:
     def __init__(self, level_data, surface):
-         self.display_surface = surface
-         self.setup_level(level_data)
+        self.display_surface = surface
+        self.setup_level(level_data)
 
     def setup_level(self, layout):
         self.tiles = pygame.sprite.Group()
@@ -14,9 +15,8 @@ class Map:
                 if cell == "X":
                     x = col_index * tile_size
                     y = row_index * tile_size
-                    tile = Tile((x,y),tile_size)
+                    tile = Tile((x, y), tile_size)
                     self.tiles.add(tile)
-
 
     def run(self):
         self.tiles.draw(self.display_surface)
---
+++ python_files/settings.py
@@ -0,0 +1,15 @@
+import pygame
+
+map_list =[
+    "XXXXXXXXXXXXXXXXXXXXXXXXX",
+    "X                       X",
+    "X              XXX      X",
+    "X                       X",
+    "X        XXX            X",
+    "X                 XXX   X",
+    "X                       X",
+    "XXXXXXXXXXXXXXXXXXXXXXXXX"]
+
+tile_size = 50
+screen_width = 1200
+screen_height = len(map_list) * tile_size
