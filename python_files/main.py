1f172ee19b0fd2d4f9542749c4489cce7fb59843
Tao <tao.spa2003@gmail.com>
2022-01-22 14:15:52 +0100

    putting python files into a specified folder + fix 'convention errors'

--- python_files/main.py
+++ python_files/main.py
@@ -6,10 +6,10 @@ from map import *
 
 # Pygame init
 pygame.init()
-screen = pygame.display.set_mode((screen_width,screen_height))
+screen = pygame.display.set_mode((screen_width, screen_height))
 clock = pygame.time.Clock()
 map = Map(map_list, screen)
-test_tile = pygame.sprite.Group(Tile((100,100),200))
+test_tile = pygame.sprite.Group(Tile((100, 100), 200))
 
 while True:
     for event in pygame.event.get():
